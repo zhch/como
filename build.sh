94dc0999ac109671588d7bb35d8257e0fa20d4b9
zhouchong <zhouchong@didichuxing.com>
2017-01-09 19:18:54 +0800

    set install prefix

--- build.sh
+++ build.sh
@@ -6,17 +6,17 @@ dir_name=`pwd`
 #######################
 echo "going to build libs"
 
+echo "going to build libev"
+cd ${dir_name}/lib/libev/libev-4.24
+./configure
+make --prefix=${dir_name}/lib/libev
+make install
+
 echo "going to build glib"
 cd ${dir_name}/lib/glib/glib-2.50.2
 pwd
 make clean
 ./autogen.sh  --prefix=${dir_name}/lib/glib --disable-libmount --with-pcre
-make
-make install
-
-echo "going to build libev"
-cd ${dir_name}/lib/libev/libev-4.24
-./configure
 make
 make install
 
